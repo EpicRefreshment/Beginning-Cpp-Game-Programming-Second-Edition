dc77be88d7274141e72f2cf67400557aa7aedb7a
Jonathan Wolford <jwolford6@login-ice-3.pace.gatech.edu>
2025-09-03 18:54:12 -0400

    deleted build folder

--- build/SFML/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Vertex.cpp.o.d
+++
@@ -1,9 +0,0 @@
-SFML/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Vertex.cpp.o: \
- /home/hice1/jwolford6/ece6122/Beginning-Cpp-Game-Programming-Second-Edition/SFML/src/SFML/Graphics/Vertex.cpp \
- /usr/include/stdc-predef.h \
- /home/hice1/jwolford6/ece6122/Beginning-Cpp-Game-Programming-Second-Edition/SFML/include/SFML/Graphics/Vertex.hpp \
- /home/hice1/jwolford6/ece6122/Beginning-Cpp-Game-Programming-Second-Edition/SFML/include/SFML/Graphics/Export.hpp \
- /home/hice1/jwolford6/ece6122/Beginning-Cpp-Game-Programming-Second-Edition/SFML/include/SFML/Config.hpp \
- /home/hice1/jwolford6/ece6122/Beginning-Cpp-Game-Programming-Second-Edition/SFML/include/SFML/Graphics/Color.hpp \
- /home/hice1/jwolford6/ece6122/Beginning-Cpp-Game-Programming-Second-Edition/SFML/include/SFML/System/Vector2.hpp \
- /home/hice1/jwolford6/ece6122/Beginning-Cpp-Game-Programming-Second-Edition/SFML/include/SFML/System/Vector2.inl
